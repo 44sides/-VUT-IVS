dc77dc696d3c2d54ceb7ca749dfcbba15a91122a
44sides <58009231+44sides@users.noreply.github.com>
2020-04-28 02:47:53 +0300

    Standart deviation script

---
+++ profiling/standart_deviation.py
@@ -0,0 +1,26 @@
+# @file standart_deviation.py
+# @brief Calculation of Standart deviation
+# @see math_library.py
+# @pre The data file consists of a number on each line and is readable
+# @post Printing of standart deviation
+
+from math_library import Math_library as math
+import fileinput
+
+N = 0
+sum_sqrt_nums = 0
+sum_nums = 0
+
+for num in fileinput.input():
+    num = float(num)
+
+    N = math.add(N, 1)
+    sum_nums = math.add(sum_nums, num)
+    sum_sqrt_nums = math.add(sum_sqrt_nums, math.pow(num, 2))
+
+mean = math.div(sum_nums, N)
+
+result = math.n_root(math.div(math.sub(sum_sqrt_nums, math.mul(N, math.pow(mean, 2))), math.sub(N, 1)), 2)
+
+print(result)
+

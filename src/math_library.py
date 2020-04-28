6a1a6097fd93e5f98b545da5eb14b7eb7741af9a
44sides <58009231+44sides@users.noreply.github.com>
2020-04-28 07:17:51 +0300

    Mathlib rewrite

--- src/math_library.py
+++ src/math_library.py
@@ -1,37 +1,35 @@
-import math
-
-class Math_library(object) :
+class Math_library(object):
 
     @staticmethod
-    def add(x, y) :
+    def add(x, y):
         return x + y
-        
+
     @staticmethod
-    def sub(x, y) :
+    def sub(x, y):
         return x - y
-    
+
     @staticmethod
-    def mul(x, y) :
+    def mul(x, y):
         return x * y
-        
+
     @staticmethod
-    def div(x, y) :
+    def div(x, y):
         return x / y
 
     @staticmethod
-    def mod(x, y) :
+    def mod(x, y):
         return x % y
 
     @staticmethod
-    def fact(x) :
+    def fact(x):
         if x < 0:
             raise ValueError
             return 0
         elif x == 0 or x == 1:
             return 1
-        else :
+        else:
             fact = 1
-            while(x > 1):
+            while (x > 1):
                 fact *= x
                 x -= 1
             if x > 0 and x < 1:
@@ -40,20 +38,14 @@ class Math_library(object) :
             return fact
 
     @staticmethod
-    def root(x) :
+    def n_root(x, y):
         if x < 0:
             raise ValueError
             return
-        return x ** 0.5
+        r_pow = Math_library.div(1, y)
+        return x ** r_pow
 
     @staticmethod
-    def pow(x , y) :
+    def pow(x, y):
         return x ** y
-
-    @staticmethod
-    def log(x, y=10) : 
-        return math.log(x , y)
-
-    @staticmethod
-    def ln(x):
-        return math.log(x, math.e)
+    

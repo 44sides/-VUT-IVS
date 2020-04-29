d750e1c9e07affb8292ebdaf61c994089c7724d1
44sides <58009231+44sides@users.noreply.github.com>
2020-04-29 07:56:40 +0300

    Fixes, update for documentation

--- src/math_library.py
+++ src/math_library.py
@@ -1,25 +1,34 @@
+## @file math_library.py
+# @brief Implementation of own math. library
+# @post Library begins to be tested
+# @package CalculatorPack
+
+
+## @class Math_library
+# @brief Own math. operations
 class Math_library(object):
 
+    ## @brief Adding
     @staticmethod
     def add(x, y):
         return x + y
 
+    ## @brief Subtraction
     @staticmethod
     def sub(x, y):
         return x - y
 
+    ## @brief Multiplying
     @staticmethod
     def mul(x, y):
         return x * y
 
+    ## @brief Division
     @staticmethod
     def div(x, y):
         return x / y
 
-    @staticmethod
-    def mod(x, y):
-        return x % y
-
+    ## @brief Factorial
     @staticmethod
     def fact(x):
         if x < 0:
@@ -29,23 +38,21 @@ class Math_library(object):
             return 1
         else:
             fact = 1
-            while (x > 1):
+            while x > 1:
                 fact *= x
                 x -= 1
-            if x > 0 and x < 1:
+            if 0 < x < 1:
                 raise ValueError
                 return
             return fact
 
+    ## @brief Common root
     @staticmethod
     def n_root(x, y):
-        if x < 0:
-            raise ValueError
-            return
         r_pow = Math_library.div(1, y)
         return x ** r_pow
 
+    ## @brief Power
     @staticmethod
     def pow(x, y):
         return x ** y
-    
